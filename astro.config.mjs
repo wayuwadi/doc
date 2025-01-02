d66d11aa598792291b1455733b538f46bdccb6e2
娃与娃 <wadi@wayuwa.cn>
2025-01-02 08:04:17 +0800

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://doc.wayuwa.cn',
 	integrations: [
 		starlight({
 			title: 'My Docs',
